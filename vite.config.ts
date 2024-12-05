dbbba48275ddb3db54f0c66370de67db897c5233
Pallos Gábor György <pallosgabor99@gmail.com>
2024-12-05 23:38:18 +0100

    rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [preact()],
-	base: "/SimpleWebshop/"  
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [preact()],
+  base: mode === 'production' ? '/SimpleWebshop/' : '/', // Use base path only in production
+}));
